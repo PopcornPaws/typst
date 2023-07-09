269fd9fe2ebacb02915c0c42dfe52f8836436832
PopcornPaws <lipschitz77@gmail.com>
2023-07-09 08:46:46 +0200

    wip

--- library/src/meta/glossary.rs
+++ library/src/meta/glossary.rs
@@ -1,6 +1,6 @@
 use crate::prelude::*;
 
-/// A glossary of acronyms, etc. TODO
+/// A glossary of acronyms, etc.
 /// Display: Glossary
 /// Category: meta
 #[element(Locatable, Synthesize, Show, Finalize, LocalName)]
@@ -8,7 +8,7 @@ pub struct GlossaryElem {
     #[required]
     #[parse(
         let Spanned { v: mut path, span } =
-            args.expect::<Spanned<BibPaths>>("path to bibliography file")?;
+            args.expect::<Spanned<GlossaryPath>>("path to glossary file")?;
         for path in &mut paths.0 {
             // resolve paths
             *path = vm.locate(path).at(span)?.to_string_lossy().into();
@@ -20,6 +20,16 @@ pub struct GlossaryElem {
     pub path: GlossaryPath,
     #[default(Some(Smart::Auto))]
     pub title: Option<Smart<Content>>,
+    // TODO style
 }
 
 pub struct GlossaryPath(EcoString);
+
+impl GlossaryElem {
+    pub fn find(introspector: Tracked<Introspector>) -> StrResult<Self> {
+        let mut iter = introspector.query(&Self::func().select()).into_iter();
+        let Some(elem) = iter.next() else {
+            return Err("the document does not contain a bibliography".into());
+        }
+    }
+}
